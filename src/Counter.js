be772a220a3390cbd3f9dcabf89df6fbe121bcb4
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-01-16 18:18:40 +0100

    Refactor Counter component to include child components and context provider

--- src/Counter.js
+++ src/Counter.js
@@ -4,12 +4,16 @@ import { createContext, useState } from "react";
 const CounterContext = createContext();
 
 // 2. Create a parent component
-function Counter() {
+function Counter({ children }) {
   const [count, setCount] = useState(0);
   const increase = () => setCount((c) => c + 1);
   const decrease = () => setCount((c) => c - 1);
 
-  return <span>Counter</span>;
+  return (
+    <CounterContext.Provider value={{ count, increase, decrease }}>
+      <span>{children}</span>
+    </CounterContext.Provider>
+  );
 }
 
 // 3. Create child components to help implementing the common task
