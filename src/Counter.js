62057b9b8454ebd7f89a732746a6c05ce2a42525
SenseiZarkoDukic <zarko.dukic@gmail.com>
2024-01-16 18:14:17 +0100

    Refactor code to improve performance and readability

--- src/Counter.js
+++ src/Counter.js
@@ -1,7 +1,9 @@
 import { createContext, useState } from "react";
 
+// 1. Create a context
 const CounterContext = createContext();
 
+// 2. Create a parent component
 function Counter() {
   const [count, setCount] = useState(0);
   const increase = () => setCount((c) => c + 1);
@@ -9,4 +11,8 @@ function Counter() {
 
   return <span>Counter</span>;
 }
+
+// 3. Create child components to help implementing the common task
+
+// 4. Add child components as properties to the parent component
 export default Counter;
